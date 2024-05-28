00e7cae42de6f596e1fb214bd052cda0a333a0bc
OrlandoMts <dani_monts19@hotmail.com>
2024-05-27 23:36:01 -0600

    6.Start module seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27018/pokemon-nest'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
