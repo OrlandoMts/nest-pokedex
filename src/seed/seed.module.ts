5a07bf8089e685effed3040ca45d211a9f3078eb
OrlandoMts <dani_monts19@hotmail.com>
2024-05-28 23:15:07 -0600

    7.Axios to fecth data and inserte in db with seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 import { SeedController } from './seed.controller';
+import { SeedService } from './seed.service';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [HttpModule, PokemonModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { PokemonItf } from './interfaces/pokemon.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Execute seed';
+  private readonly logger = new Logger(SeedService.name);
+
+  constructor(
+    private readonly httpSrv: HttpService,
+    private readonly pokemonSrv: PokemonService,
+  ) {}
+
+  async executeSeed(): Promise<any> {
+    const { data } = await firstValueFrom(
+      this.httpSrv
+        .get<PokemonItf>('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .pipe(
+          catchError((error: AxiosError) => {
+            this.logger.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      const body = { name, no };
+      try {
+        await this.pokemonSrv.create(body);
+      } catch (error: any) {
+        throw new InternalServerErrorException(
+          `Cant create Pokemon - Check server logs`,
+        );
+      }
+    });
+    return { ok: true, msg: 'Se insertaron correctamente' };
   }
 }
